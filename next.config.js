61125d48e5f78c2870df90ae502dd0f060391177
real-folkblues <marenicolor@gmail.com>
2024-03-06 18:39:12 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com','avatars.githubusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  }
+  /* config options here */
+}
+
 module.exports = nextConfig
